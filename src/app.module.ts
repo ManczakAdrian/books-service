bd16c1d23cfeff56660b0a58493584ce3294fb6f
Adrian Manczak <manczakadrian@gmail.com>
2024-06-01 14:20:40 +0200

    getByEmail_update

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,12 +10,13 @@ import { AppService } from './app.service';
 import { AuthorsModule } from './authors/authors.module';
 import { UsersModule } from './users/users.module';
 import { UsersService } from './users/users.service';
+import { PrismaService } from './shared/services/prisma.service';
 
 
 @Module({
   imports: [AuthorsModule,  UsersModule, ],
   controllers: [AppController],
-  providers: [AppService, UsersService, ],
+  providers: [AppService, UsersService, PrismaService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
